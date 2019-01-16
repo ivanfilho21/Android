be31849fe1cd18866832ae5241df2de3191176a6
ivanfilho21 <ivanfilho21@gmail.com>
2019-01-16 12:45:35 -0300

    Added the operation enum and a method to verify the operation.

--- Calculator/app/src/main/java/com/example/ivan/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/ivan/calculator/MainActivity.java
@@ -22,12 +22,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         operationLabel = findViewById(R.id.operationLabel);
         resultLabel = findViewById(R.id.resultLabel);
 
+        initialize();
+    }
 
+    private void initialize() {
         operation = Operation.None;
         digitMemory = 0;
         resultMemory = 0;
     }
-
 
     @Override
     public void onClick(View v) {
@@ -44,20 +46,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             }
         }
+
         // found it, print text or append.
         if (index != -1) {
-            String s = operationLabel.getText().toString();
-            if (s.isEmpty() || s.equalsIgnoreCase("0"))
-                operationLabel.setText(butText);
-            else {
-                s += butText;
-                operationLabel.setText(s);
+            assignNumber(butText);
+            return;
+        }
+
+        for (int i = 0; i < operands.length; i++) {
+            if (butText.equalsIgnoreCase(operands[i])) {
+                index = i;
+                break;
             }
+        }
+
+        if (index != -1) {
+            assignOperation(butText);
             return;
         }
 
         if (butText.equalsIgnoreCase(getString(R.string.clear_button)))
-            operationLabel.setText("0");
+            clear();
+    }
 
+    private void assignNumber(String number) {
+        String s = operationLabel.getText().toString();
+        if (s.isEmpty() || s.equalsIgnoreCase("0")) {
+            s = "" + number;
+        }
+        else {
+            s += "" + number;
+        }
+        operationLabel.setText(s);
+    }
+
+    private void assignOperation(String operation) {
+        final String plus = getString(R.string.plus);
+        final String minus = getString(R.string.minus);
+        final String times = getString(R.string.times);
+        final String obelus = getString(R.string.obelus);
+
+        if (operation.equalsIgnoreCase(plus))
+            this.operation = Operation.Addition;
+        else if (operation.equalsIgnoreCase(minus))
+            this.operation = Operation.Subtraction;
+        else if (operation.equalsIgnoreCase(times))
+            this.operation = Operation.Multiplication;
+        else if (operation.equalsIgnoreCase(obelus))
+            this.operation = Operation.Division;
+
+        String s = operationLabel.getText() + this.operation.getValue();
+        operationLabel.setText(s);
+    }
+
+    private void doOperation() {
+
+        String s = operationLabel.getText().toString();
+        String[] a = s.split(operation.getValue());
+
+        for (int i = 0; i < a.length; i++) {
+            // Todo: calculate all operands
+        }
+    }
+
+    private void clear() {
+        initialize();
+        operationLabel.setText("0");
     }
 }
--- Calculator/app/src/main/java/com/example/ivan/calculator/Operation.java
+++ Calculator/app/src/main/java/com/example/ivan/calculator/Operation.java
@@ -1,5 +1,21 @@
 package com.example.ivan.calculator;
 
+import android.content.res.Resources;
+
 public enum Operation {
-    None, Addition, Subtraction, Multiplication, Division
+    None(""),
+    Addition("+"),
+    Subtraction("−"),
+    Multiplication("×"),
+    Division("÷");
+
+    String value;
+
+    Operation(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
