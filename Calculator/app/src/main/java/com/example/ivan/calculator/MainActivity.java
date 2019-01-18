4379d8b5427d94156c236f2429360d7d807428c3
ivanfilho21 <ivanfilho21@gmail.com>
2019-01-18 18:00:32 -0300

    Finished the project.

--- Calculator/app/src/main/java/com/example/ivan/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/ivan/calculator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ivan.calculator;
 
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,45 +17,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Operation operation;
     private int currentOperandIndex;
-    private double operand1, operand2, resultMemory;
-    private TextView operand1Label, operand2Label, operatorLabel, resultLabel;
+    private double[] operands;
+    private TextView[] operandLabels;
+    private TextView operatorLabel, resultLabel;
     private Drawable background, default_background;
+    private boolean canAppend;
 
-    DecimalFormat decimalFormat = new DecimalFormat("#.##");
+    private final int MAX_LENGTH = 15;
+
+    private DecimalFormat decimalFormat = new DecimalFormat("#.###############");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(getTitle());
-        }
-        //toolbar.setSubtitle("");
-        toolbar.inflateMenu(R.menu.menu);*/
 
-        operand1Label = findViewById(R.id.operand1);
-        operand2Label = findViewById(R.id.operand2);
+        operandLabels = new TextView[2];
+        operandLabels[0] = findViewById(R.id.operand1);
+        operandLabels[1] = findViewById(R.id.operand2);
+
         operatorLabel = findViewById(R.id.operatorLabel);
         resultLabel = findViewById(R.id.result);
 
-        operand1Label.setOnClickListener(new View.OnClickListener() {
+        operandLabels[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setOperandIndex(0);
             }
         });
-        operand2Label.setOnClickListener(new View.OnClickListener() {
+        operandLabels[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setOperandIndex(1);
             }
         });
 
-        background = operand1Label.getBackground();
-        default_background = operand2Label.getBackground();
+        background = operandLabels[0].getBackground();
+        default_background = operandLabels[1].getBackground();
 
         initialize();
     }
@@ -82,6 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 return true;
             case R.id.about:
+                showAlertDialog(getString(R.string.about_message), getString(R.string.about));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -90,62 +91,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initialize() {
         resetOperandIndex();
-        resultMemory = 0;
 
-        operand1 = 0;
-        operand2 = Double.NaN;
+        operands = new double[2];
+        operands[0] = 0;
+        operands[1] = Double.NaN;
 
-        String s = convertToFormat(operand1);
-        operand1Label.setText(s);
+        String s = convertToFormat(operands[0]);
+        operandLabels[0].setText(s);
 
-        if (Double.isNaN(operand2)) operand2Label.setText(null);
-        else operand2Label.setText(s);
+        if (Double.isNaN(operands[1])) operandLabels[1].setText(null);
+        else operandLabels[1].setText(s);
 
         resultLabel.setText(null);
 
         assignOperation(Operation.None.value);
+        canAppend = true;
     }
 
     @Override
     public void onClick(View v) {
         Button b = (Button) v;
-        String butText = b.getText().toString();
+        String buttonText = b.getText().toString();
 
-        String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}; //, getString(R.string.plus), getString(R.string.minus), getString(R.string.times), getString(R.string.obelus)};
+        String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         String[] operands = {getString(R.string.plus), getString(R.string.minus), getString(R.string.times), getString(R.string.obelus)};
 
         int index = -1;
+
         for (int i = 0; i < numbers.length; i++) {
-            if (butText.equalsIgnoreCase(numbers[i])) {
+            if (buttonText.equalsIgnoreCase(numbers[i])) {
                 index = i;
                 break;
             }
         }
 
         if (index != -1) {
-            assignNumber(butText);
+            setCurrentOperand(Double.parseDouble(buttonText));
             return;
         }
 
         for (int i = 0; i < operands.length; i++) {
-            if (butText.equalsIgnoreCase(operands[i])) {
+            if (buttonText.equalsIgnoreCase(operands[i])) {
                 index = i;
                 break;
             }
         }
 
         if (index != -1) {
-            assignOperation(butText);
+            assignOperation(buttonText);
             return;
         }
 
-        if (butText.equalsIgnoreCase(getString(R.string.clear_button)))
+        if (buttonText.equalsIgnoreCase(getString(R.string.clear_button)))
             clear();
-        else if (butText.equalsIgnoreCase(getString(R.string.clear_entry_button)))
+        else if (buttonText.equalsIgnoreCase(getString(R.string.clear_entry_button)))
             clearEntry();
-        else if (butText.equalsIgnoreCase(getString(R.string.del_button)))
-            clear();
-        else if (butText.equalsIgnoreCase(getString(R.string.result_button)))
+        else if (buttonText.equalsIgnoreCase(getString(R.string.del_button)))
+            delete();
+        else if (buttonText.equalsIgnoreCase(getString(R.string.result_button)))
             doOperation();
     }
 
@@ -153,91 +156,115 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return decimalFormat.format(value);
     }
 
-    private void clear() {
-        initialize();
-    }
-
-    private void clearEntry() {
-        setCurrentOperand(0, false);
-    }
-
     private void setOperandIndex(int i) {
         currentOperandIndex = i;
 
         if (currentOperandIndex == 0) {
-            operand1Label.setBackgroundDrawable( background );
-            operand2Label.setBackgroundDrawable( default_background );
+            operandLabels[0].setBackgroundDrawable( background );
+            operandLabels[1].setBackgroundDrawable( default_background );
         }
         else{
-            operand1Label.setBackgroundDrawable( default_background );
-            operand2Label.setBackgroundDrawable( background );
+            operandLabels[0].setBackgroundDrawable( default_background );
+            operandLabels[1].setBackgroundDrawable( background );
         }
+
+        canAppend = false;
     }
 
     private void resetOperandIndex() {
         setOperandIndex(0);
     }
 
-    private void toggleOperandIndex() {
-        if (currentOperandIndex == 0) setOperandIndex(1);
-        else setOperandIndex(0);
-    }
-
-    private void setCurrentOperand(double n, boolean append) {
+    private void setCurrentOperand(double n) {
+        TextView operandLabel = operandLabels[currentOperandIndex];
         String text = "";
 
-        if (currentOperandIndex == 0) {
-            if (append)
-                text = operand1Label.getText().toString();
-            text += convertToFormat(n);
+        if (operandLabel.length() >= MAX_LENGTH) return;
 
-            operand1Label.setText(text);
-            operand1 = Double.parseDouble(operand1Label.getText().toString());
-        }
-        else {
-            if (append)
-                text = operand2Label.getText().toString();
-            text += convertToFormat(n);
+        if (!operandLabel.getText().toString().isEmpty() && Double.parseDouble(operandLabel.getText().toString()) == 0)
+            operandLabel.setText("");
 
-            operand2Label.setText(text);
-            operand2 = Double.parseDouble(operand2Label.getText().toString());
-        }
-    }
+        if (canAppend)
+            text = operandLabel.getText().toString();
+        text += convertToFormat(n);
 
-    private double getCurrentOperand() {
-        return currentOperandIndex == 0 ? operand1 : operand2;
-    }
+        operandLabel.setText(text);
 
-    private void assignNumber(String buttonText) {
-        boolean append = Double.compare(getCurrentOperand(), 0) != 0;
-        setCurrentOperand(Double.parseDouble(buttonText), append);
+        operands[currentOperandIndex] = Double.parseDouble(operandLabel.getText().toString());
+
+        canAppend = true;
     }
 
     private void assignOperation(String operationText) {
         operation = Operation.getOperation(operationText);
         operatorLabel.setText(operationText);
-        if (operation != Operation.None)
+        if (operation != Operation.None) {
             setOperandIndex(1);
+        }
     }
 
     private void doOperation() {
-        if (Double.isNaN(operand2)) {
-            if (operation == Operation.Multiplication || operation == Operation.Division) {
-                operand2 = 1;
-            }
-            else {
-                operand2 = 0;
-            }
+        double o1 = operands[0];
+        double o2 = operands[1];
+
+        if (Double.isNaN(operands[1])) {
+            if (operation == Operation.Multiplication || operation == Operation.Division)
+                o2 = 1;
+            else
+                o2 = 0;
         }
 
         double r = 0;
+
         switch (operation) {
-            case Addition: r = operand1 + operand2; break;
-            case Subtraction: r = operand1 - operand2; break;
-            case Multiplication: r = operand1 * operand2; break;
-            case Division: r = operand1 / operand2; break;
+            case Addition: r = o1 + o2; break;
+            case Subtraction: r = o1 - o2; break;
+            case Multiplication: r = o1 * o2; break;
+            case Division:
+                if (o2 == 0) {
+                    resultLabel.setText(getString(R.string.cannot_divide_by_zero));
+                    return;
+                }
+                r = o1 / o2; break;
             case None: resultLabel.setText(null); return;
         }
         resultLabel.setText(convertToFormat(r));
+        canAppend = false;
+    }
+
+    private void clear() {
+        initialize();
+    }
+
+    private void clearEntry() {
+        canAppend = false;
+        setCurrentOperand(0);
+    }
+
+    private void delete() {
+        TextView operandLabel = operandLabels[currentOperandIndex];
+        String t = operandLabel.getText().toString();
+
+        if (t.length() > 1)
+            t = t.substring(0, t.length() -1);
+        else {
+            t = convertToFormat(0);
+        }
+        operandLabel.setText(t);
+        canAppend = false;
+        setCurrentOperand(Double.parseDouble(t));
+    }
+
+    private void showAlertDialog(String message, String title) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 }
